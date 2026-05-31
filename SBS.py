2437398817e9dec3a1a0507c0b7060ac1d1e7832
stephanie-0210 <2425404168@live.gctu.edu.gh>
2026-05-31 23:54:49 +0000

    Add files via upload

---
+++ SBS.py
@@ -0,0 +1,56 @@
+# File: SBS.py
+# Description:Print out my initials
+# Assignment Number: 1
+#
+# Name:Stephanie Bih Song
+# STUDENT ID: 2425404168
+# Email:2425404168@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Stephanie Bih Song, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+
+def main():
+    # Print the required small initials and large block initials.
+    s1 = "SSSSSSSS...."
+    s2 = "SSSSSSSSSS.."
+    s3 = "SSSS........"
+    s4 = "SSSS........"
+    s5 = "..SSSSSS...."
+    s6 = "..SSSSSS...."
+    s7 = "........SSSS"
+    s8 = "........SSSS"
+    s9 = "SSSSSSSSSS.."
+    s10 = "SSSSSSSS...."
+
+    b1 = "BBBBBBBB...."
+    b2 = "BB......BB.."
+    b3 = "BB......BB.."
+    b4 = "BBBBBBBB...."
+    b5 = "BB......BB.."
+    b6 = "BB......BB.."
+    b7 = "BB......BB.."
+    b8 = "BB......BB.."
+    b9 = "BBBBBBBBBB.."
+    b10 = "BBBBBBBB...."
+
+    print()
+    print("...SBS")
+    print()
+    print("..." + s1 + "..." + ".." + "..." + b1 + "..." + ".." + "..." + s1 + "..." + "..")
+    print("..." + s2 + "..." + ".." + "..." + b2 + "..." + ".." + "..." + s2 + "..." + "..")
+    print("..." + s3 + "..." + ".." + "..." + b3 + "..." + ".." + "..." + s3 + "..." + "..")
+    print("..." + s4 + "..." + ".." + "..." + b4 + "..." + ".." + "..." + s4 + "..." + "..")
+    print("..." + s5 + "..." + ".." + "..." + b5 + "..." + ".." + "..." + s5 + "..." + "..")
+    print("..." + s6 + "..." + ".." + "..." + b6 + "..." + ".." + "..." + s6 + "..." + "..")
+    print("..." + s7 + "..." + ".." + "..." + b7 + "..." + ".." + "..." + s7 + "..." + "..")
+    print("..." + s8 + "..." + ".." + "..." + b8 + "..." + ".." + "..." + s8 + "..." + "..")
+    print("..." + s9 + "..." + "**" + "..." + b9 + "..." + "**" + "..." + s9 + "..." + "**")
+    print("..." + s10 + "..." + "**" + "..." + b10 + "..." + "**" + "..." + s10 + "..." + "**")
+    print()
+
+
+main()
